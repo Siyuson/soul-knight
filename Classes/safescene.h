5438e24dc63ac83166628226333c5f1ce4ca3df9
mogiven <83754826+mogiven@users.noreply.github.com>
2021-06-08 11:06:34 +0800

    add Knight & safescene

---
+++ Classes/safescene.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef _SAfESCENE_H
+#define _SAfESCENE_H
+#include "cocos2d.h"
+#include "Knight.h"
+
+
+USING_NS_CC;
+
+class SafeScene : public Scene {
+public:
+	static Scene* createScene();
+	virtual bool init();
+	virtual void update(float delta);
+	CREATE_FUNC(SafeScene);
+private:
+	void initBG();
+
+private:
+	Sprite* bgSprite1 = nullptr;
+	Sprite* bgSprite2 = nullptr;
+	Knight* knight;
+
+};
+
+#endif
